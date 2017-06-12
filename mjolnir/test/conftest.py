f9211d4ccb9d5e827a60cfbefe84293930993b24
Erik Bernhardson <ebernhardson@wikimedia.org>
2017-06-12 16:01:33 -0700

    Move requests session mocking to conftest.py
    The code for query normalization is also going to need these
    recorded http request fixtures, so move them into conftest
    to be reusable between tests.
    
    Change-Id: I01144d66698a76e782ae418f99aa822149a93340

--- mjolnir/test/conftest.py
+++ mjolnir/test/conftest.py
@@ -1,10 +1,14 @@
 import findspark
 findspark.init()  # must happen before importing pyspark
 
-import pytest  # noqa: E402
+import hashlib  # noqa: E402
 import logging  # noqa: E402
+import os  # noqa: E402
 from pyspark import SparkContext, SparkConf  # noqa: E402
 from pyspark.sql import HiveContext  # noqa: E402
+import pytest  # noqa: E402
+import requests  # noqa: E402
+import sqlite3  # noqa: E402
 
 
 def quiet_log4j():
@@ -48,3 +52,56 @@ def hive_context(spark_context):
         HiveContext for tests
     """
     return HiveContext(spark_context)
+
+
+@pytest.fixture()
+def make_requests_session():
+    def f(path):
+        return MockSession(path)
+    return f
+
+
+class MockSession(object):
+    def __init__(self, fixture_file):
+        self._session = None
+        if fixture_file[0] != '/':
+            dir_path = os.path.dirname(os.path.realpath(__file__))
+            fixture_file = os.path.join(dir_path, fixture_file)
+        # Use sqlite for storage so we don't have to figure out how
+        # multiple pyspark executors write to the same file
+        self.sqlite = sqlite3.connect(fixture_file)
+        self.sqlite.execute(
+            "CREATE TABLE IF NOT EXISTS requests " +
+            "(digest text PRIMARY KEY, status_code int, content text)")
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def get(self, url, data=None):
+        md5 = hashlib.md5()
+        md5.update(url)
+        md5.update(data)
+        digest = md5.hexdigest()
+
+        for row in self.sqlite.execute("SELECT status_code, content from requests WHERE digest=?", [digest]):
+            return MockResponse(row[0], row[1])
+
+        r = requests.get(url, data=data)
+
+        try:
+            self.sqlite.execute("INSERT INTO requests VALUES (?,?,?)", [digest, r.status_code, r.text])
+            self.sqlite.commit()
+        except sqlite3.IntegrityError:
+            # inserted elsewhere? no big deal
+            pass
+
+        return MockResponse(r.status_code, r.text)
+
+
+class MockResponse(object):
+    def __init__(self, status_code, text):
+        self.status_code = status_code
+        self.text = text
--- mjolnir/test/test_features.py
+++ mjolnir/test/test_features.py
@@ -1,62 +1,11 @@
-import hashlib
 import mjolnir.features
-import os
 import pyspark.sql
-import requests
-import sqlite3
 
 
-class MockSession(object):
-    def __init__(self, fixture_file):
-        self._session = None
-        if fixture_file[0] != '/':
-            dir_path = os.path.dirname(os.path.realpath(__file__))
-            fixture_file = os.path.join(dir_path, fixture_file)
-        # Use sqlite for storage so we don't have to figure out how
-        # multiple pyspark executors write to the same file
-        self.sqlite = sqlite3.connect(fixture_file)
-        self.sqlite.execute(
-            "CREATE TABLE IF NOT EXISTS requests " +
-            "(digest text PRIMARY KEY, status_code int, content text)")
+def test_collect(spark_context, hive_context, make_requests_session):
+    def session_factory():
+        return make_requests_session('fixtures/requests/test_features.sqlite3')
 
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
-
-    def get(self, url, data=None):
-        md5 = hashlib.md5()
-        md5.update(url)
-        md5.update(data)
-        digest = md5.hexdigest()
-
-        for row in self.sqlite.execute("SELECT status_code, content from requests WHERE digest=?", [digest]):
-            return MockResponse(row[0], row[1])
-
-        r = requests.get(url, data=data)
-
-        try:
-            self.sqlite.execute("INSERT INTO requests VALUES (?,?,?)", [digest, r.status_code, r.text])
-            self.sqlite.commit()
-        except sqlite3.IntegrityError:
-            # inserted elsewhere? no big deal
-            pass
-
-        return MockResponse(r.status_code, r.text)
-
-
-class MockResponse(object):
-    def __init__(self, status_code, text):
-        self.status_code = status_code
-        self.text = text
-
-
-def session_factory():
-    return MockSession('fixtures/requests/test_features.sqlite3')
-
-
-def test_collect(spark_context, hive_context):
     r = pyspark.sql.Row('wikiid', 'query', 'hit_page_id')
     source_data = {
         'apple': [18978754, 36071326, 856],
