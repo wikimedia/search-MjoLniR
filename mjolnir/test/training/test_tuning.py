84f281cb3eeaceb3ebcf43f08f28cf1e68f3be83
Erik Bernhardson <ebernhardson@wikimedia.org>
2017-06-23 14:05:06 -0700

    Split hyperopt integration into its own file
    The hyperopt integration is getting more and more complex, and an
    upcoming patch to run multiple cross-validations in parallel only makes
    it even more complex. Make the code a bit clearer by splitting out
    hyperopt specific code into its own file.
    
    Change-Id: If5400b8183e31eaf0d56fe50899d196b9f5e1314

--- mjolnir/test/training/test_tuning.py
+++ mjolnir/test/training/test_tuning.py
@@ -1,4 +1,3 @@
-import hyperopt
 import mjolnir.training.tuning
 import mjolnir.training.xgboost
 from pyspark.sql import functions as F
@@ -58,68 +57,3 @@ def test_cross_validate_plain_df(df_train):
         num_folds=2, num_fold_partitions=1, num_cv_jobs=1, num_workers=1)
     # one score for each fold
     assert len(scores) == 2
-
-
-def test_hyperopt(df_train):
-    "Not an amazing test...basically sees if the happy path doesnt blow up"
-    space = {
-        'num_rounds': 50,
-        'max_depth': hyperopt.hp.quniform('max_depth', 1, 20, 1)
-    }
-
-    # mostly hyperopt just calls cross_validate, of which the integration with
-    # xgboost is separately tested. Instead of going all the way into xgboost
-    # mock it out w/MockModel.
-    best_params, trails = mjolnir.training.tuning.hyperopt(
-        df_train, MockModel, space, max_evals=5, num_folds=2,
-        num_fold_partitions=1, num_cv_jobs=1, num_workers=1)
-    assert isinstance(best_params, dict)
-    # num_rounds should have been unchanged
-    assert 'num_rounds' in best_params
-    assert best_params['num_rounds'] == 50
-    # should have max_evals evaluations
-    assert len(trails.trials) == 5
-
-
-def test_gridsearch(df_train):
-    space = {
-        'num_rounds': 50,
-        'max_depth': hyperopt.hp.choice('max_depth', [10, 20, 30]),
-    }
-
-    gen = MockModelGen()
-    best_params, trials = mjolnir.training.tuning.grid_search(
-        df_train, gen, space, num_folds=2,
-        num_fold_partitions=1, num_cv_jobs=1, num_workers=1)
-    assert isinstance(best_params, dict)
-    assert 'num_rounds' in best_params
-    # num rounds should be unchanged
-    assert best_params['num_rounds'] == 50
-    # should have 3 iterations for the 3 max depth's
-    assert len(trials.trials) == 3
-    param_depths = sorted([param['max_depth'] for param in gen.params])
-    # TODO: Why is this called 2x as many times as expected?
-    # For some reason the correct number of trials is still
-    # returned though.
-    assert [10, 10, 20, 20, 30, 30] == param_depths
-
-
-class MockModelGen(object):
-    def __init__(self):
-        self.params = []
-
-    def __call__(self, df, params, num_workers):
-        self.params.append(params)
-        return MockModel(df, params, num_workers)
-
-
-class MockModel(object):
-    def __init__(self, df, params, num_workers):
-        # Params that were passed to hyperopt
-        assert isinstance(params, dict)
-        assert 'max_depth' in params
-        assert params['num_rounds'] == 50
-        assert num_workers == 1
-
-    def eval(self, df_test, j_groups=None, feature_col='features', label_col='label'):
-        return 1.0
--- mjolnir/training/xgboost.py
+++ mjolnir/training/xgboost.py
@@ -1,6 +1,7 @@
+from __future__ import absolute_import
 import hyperopt
 import mjolnir.spark
-import mjolnir.training.tuning
+import mjolnir.training.hyperopt
 import numpy as np
 import pprint
 import pyspark.sql
@@ -442,7 +443,7 @@ def tune(df, num_folds=5, num_fold_partitions=100, num_cv_jobs=5, num_workers=5,
     """
     def eval_space(space, max_evals):
         """Eval a space using standard hyperopt"""
-        best, trials = mjolnir.training.tuning.hyperopt(
+        best, trials = mjolnir.training.hyperopt.minimize(
             df, train, space, max_evals=max_evals,
             num_folds=num_folds, num_fold_partitions=num_fold_partitions,
             num_cv_jobs=num_cv_jobs, num_workers=num_workers)
@@ -453,7 +454,7 @@ def tune(df, num_folds=5, num_fold_partitions=100, num_cv_jobs=5, num_workers=5,
 
     def eval_space_grid(space):
         """Eval all points in the space via a grid search"""
-        best, trials = mjolnir.training.tuning.grid_search(
+        best, trials = mjolnir.training.hyperopt.grid_search(
             df, train, space, num_folds=num_folds, num_fold_partitions=num_fold_partitions,
             num_cv_jobs=num_cv_jobs, num_workers=num_workers)
         for k, v in space.items():
