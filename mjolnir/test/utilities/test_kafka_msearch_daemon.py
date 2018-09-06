99c31c68c071d1bc65cf9563ea573f800703bb35
Erik Bernhardson <ebernhardson@wikimedia.org>
2018-09-06 13:12:57 -0700

    Remove spurious whitespace
    Change-Id: I906796180b97cf1d92e574165fdac41004ff6446

--- mjolnir/test/utilities/test_kafka_msearch_daemon.py
+++ mjolnir/test/utilities/test_kafka_msearch_daemon.py
@@ -1,11 +1,10 @@
 import inspect
 
 from mjolnir.utilities.kafka_msearch_daemon import arg_parser
-import mjolnir.kafka.msearch_daemon
+from mjolnir.kafka.msearch_daemon import Daemon
+
 
 def test_args_match_init():
     kwargs = dict(vars(arg_parser().parse_args(['-b', 'localhost:9092'])))
-    available = set(inspect.getargspec(mjolnir.kafka.msearch_daemon.Daemon.__init__).args[1:])
+    available = set(inspect.getargspec(Daemon.__init__).args[1:])
     assert all(k in available for k in kwargs.keys())
-
-
