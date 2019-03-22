9e5c38cab1ebf8854fd552cf6ba9c93312c23571
Erik Bernhardson <ebernhardson@wikimedia.org>
2019-03-22 13:36:43 -0700

    Use normalized `level` field in structured logging
    By default python logging only makes available a levelname
    field with the nice INFO, WARN, etc. labels. Extend the
    formatter used to put this in the `level` field which is
    being standardized on in WMF logging infrastrucre.
    
    Bug: T218833
    Change-Id: Ib733480d99f0b5d33ce7810e4e5c3ee1ed4b83b8

--- mjolnir/logging.py
+++ mjolnir/logging.py
@@ -11,8 +11,14 @@ import logging
 from pythonjsonlogger.jsonlogger import JsonFormatter
 
 
+class CustomJsonFormatter(JsonFormatter):
+    def add_fields(self, log_record, record, message_dict):
+        super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
+        log_record['level'] = record.levelname.upper()
+
+
 class StructuredLoggingHandler(logging.StreamHandler):
     def __init__(self, fmt, rsyslog=False):
         super(StructuredLoggingHandler, self).__init__()
         prefix = '@cee' if rsyslog else ''
-        self.formatter = JsonFormatter(fmt=fmt, prefix=prefix)
+        self.formatter = CustomJsonFormatter(fmt=fmt, prefix=prefix)
