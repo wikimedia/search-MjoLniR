c00759534557bf1ee15085a62060cfe6b7088515
Erik Bernhardson <ebernhardson@wikimedia.org>
2020-01-07 10:15:58 -0800

    Allow cli to load without pyspark
    The daemons dont have pyspark installed, but recent adjustments to the
    cli commands made it required. Gracefully detect missing pyspark and
    don't offer the commands if it is not installed.
    
    Change-Id: I0773c070dccdfd1deaa4ac94c20e7b3d6d484207

--- mjolnir/cli/__init__.py
+++ mjolnir/cli/__init__.py
@@ -1,23 +1,33 @@
 from argparse import ArgumentParser
-from typing import cast, Callable, Mapping
+from typing import cast, Callable, Dict
 
-from . import (
-    dbn, feature_selection, feature_vectors, hyperparam,
-    kafka_bulk_daemon, kafka_msearch_daemon, make_folds,
-    norm_query_clustering, query_clicks_ltr, train
-)
+from . import kafka_bulk_daemon, kafka_msearch_daemon
 
 Factory = Callable[[ArgumentParser], Callable[..., None]]
-CLI_COMMANDS = cast(Mapping[str, Factory], {
-    'dbn': dbn.configure,
-    'feature_selection': feature_selection.configure,
-    'feature_vectors': feature_vectors.configure,
-    'hyperparam': hyperparam.configure,
+CLI_COMMANDS = cast(Dict[str, Factory], {
     'kafka_bulk_daemon': kafka_bulk_daemon.configure,
     'kafka_daemon': kafka_bulk_daemon.configure,
     'kafka_msearch_daemon': kafka_msearch_daemon.configure,
-    'make_folds': make_folds.configure,
-    'norm_query_clustering': norm_query_clustering.configure,
-    'query_clicks_ltr': query_clicks_ltr.configure,
-    'train': train.configure,
 })
+
+try:
+    # None of these work if pyspark is unavailable, and we don't
+    # want to force pyspark to be available for the kafka daemons
+    from . import (
+        dbn, feature_selection, feature_vectors, hyperparam,
+        make_folds, norm_query_clustering, query_clicks_ltr, train
+    )
+
+    CLI_COMMANDS.update({
+        'dbn': dbn.configure,
+        'feature_selection': feature_selection.configure,
+        'feature_vectors': feature_vectors.configure,
+        'hyperparam': hyperparam.configure,
+        'make_folds': make_folds.configure,
+        'norm_query_clustering': norm_query_clustering.configure,
+        'query_clicks_ltr': query_clicks_ltr.configure,
+        'train': train.configure,
+    })
+except ImportError as e:
+    if e.name is None or not e.name.startswith('pyspark'):
+        raise
