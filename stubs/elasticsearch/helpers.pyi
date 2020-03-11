f47508b8a81512d5f4a373c9f8b28efc3314a23f
Erik Bernhardson <ebernhardson@wikimedia.org>
2020-03-11 16:41:56 -0700

    bulk_daemon: Remove parallelism from bulk import
    The bulk daemons appear to be deadlocking. Stack traces while
    the daemons are stuck indicates all threads waiting on either
    a lock or a queue (or serving prometheus). Switch to the single
    threaded bulk import implementation to verify this is the case.
    
    While parallelism isn't a linear speedup, if it was this change would
    mean we now need 2 weeks to import 1 week of updates. This is
    a test, but cannot be the complete solution.
    
    Change-Id: I508afb7680af0e9d92cfdde18be3351549cde98a

--- stubs/elasticsearch/helpers.pyi
+++ stubs/elasticsearch/helpers.pyi
@@ -2,8 +2,8 @@ from elasticsearch import Elasticsearch
 from typing import Callable, Dict, Generator, Tuple
 
 
-def parallel_bulk(
-    client: Elasticsearch, actions, thread_count: int=...,
+def streaming_bulk(
+    client: Elasticsearch, actions,
     chunk_size: int=..., max_chunk_bytes: int=...,
     queue_size: int=..., expand_action_callback=Callable,
     **kwargs
